2267814de4eb2f3bb6ab16ce6a171daab3b1bb82
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-22 10:19:22 -0700

    nice

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const User = require('./model/user')
 mongoose.connect('mongodb://127.0.0.1:27017/login-demo', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 
