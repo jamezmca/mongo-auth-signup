074806ee90e29376f1c1e6664349009b19ef5649
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-21 21:54:29 -0700

    app working with proper routing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8002
@@ -8,9 +9,16 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname + '/public' })
 })
 
-app.get('/:file', (req, res) => {
+app.get('/:file', async (req, res) => {
     const { file } = req.params
-    res.sendFile(`${file}.html`, { root: __dirname + '/public' })
+    try {
+        return fs.existsSync(`./public/${file}.html`) ?
+            res.sendFile(`${file}.html`, { root: __dirname + '/public' }) :
+            res.send('<a href="/">Page not found | Return home</a>')
+    } catch (err) {
+        console.log(err)
+        res.send('<a href="/">Page not found | Return home</a>')
+    }
 })
 
 
