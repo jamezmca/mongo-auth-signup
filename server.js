d42bf6b4b262c8fffcfe526850c3f41fb12b7056
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-21 21:35:15 -0700

    app configured to use routes instead of filenames

--- server.js
+++ server.js
@@ -4,24 +4,14 @@ const port = process.env.PORT || 8002
 
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile('james.html', { root: __dirname + '/public' })
-// })
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + '/public' })
+})
 
-// app.get('/:file', (req, res) => {
-//     const { file } = req.params
-//     res.sendFile(`${file}.html`, { root: __dirname + '/public' })
-// })
-// app.get('/', (req, res) => {
-//     // res.send()
-//     res.sendFile('index.html')
-// })
-
-// app.get('/:file', (req, res) => {
-//     const { file } = req.params
-//     res.send()
-//     // res.sendFile(`${file}.html`)
-// })
+app.get('/:file', (req, res) => {
+    const { file } = req.params
+    res.sendFile(`${file}.html`, { root: __dirname + '/public' })
+})
 
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`))
