155b66b006285d574e7944736a0501e7a1111e19
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-21 21:32:46 -0700

    basic working with routing
    the app works by routing between html files with the app.use(express.static('public'))
    --this line comes just before app.listen

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8002
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 // app.get('/', (req, res) => {
 //     res.sendFile('james.html', { root: __dirname + '/public' })
