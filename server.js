e19d850fc19b6da1d159ebd2ef8a967f84b24bfa
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-22 14:04:38 -0700

    jwt created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8002
@@ -15,11 +16,33 @@ mongoose.connect('mongodb://127.0.0.1:27017/login-demo', {
 //Middlewares
 app.use(express.static(__dirname + '/public')) //if no other files, will serve index.html
 app.use(express.json())
+//JWT -> client proves itself on request & don't have to keep a stored state
 
 //Routes 
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body
+
+    const user = await User.findOne({ username }).lean()
+
+    if (!username || !password || !await bcrypt.compare(password, user.password)) {
+        return res.json({ status: 'error', error: 'Invalid username or password' })
+    } //guard clause. If this passes, then we get to successful code
+
+    //username passowrd combo is successful
+    const token = jwt.sign({
+        id: user._id, username: user.username
+    }, process.env.JWT_SECRET)
+
+    res.json({ status: 'ok', data: token })
+})
+
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     const { username, password } = req.body
+    if (!username || !password || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Incorrectly username' }) //otherwise status and data keys
+    }
+    // could also implement password checking
 
     //Hashing the passwords
     const encryptedPass = await bcrypt.hash(password, 10)
@@ -33,7 +56,13 @@ app.post('/api/register', async (req, res) => {
         res.status(200).json({ status: 'success' })
 
     } catch (err) {
-        res.json({ status: 'error' })
+        // console.log(JSON.stringify(err))
+        if (err.code === 11000) {
+            //key already exists
+            return res.json({ status: 'error', error: 'Username already exists' })
+        }
+        throw err
+        // res.json({ status: 'error' })
     }
 })
 
